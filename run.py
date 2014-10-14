44f0b0b4893eb18a42f2ca04354399f2c99bbce1
Ziwei Ye <calvin@yuugu.re>
2014-10-13 23:42:43 -0400

    How to implement findname etc here?

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
 import google
 import re
+    
+def soupify(webtext):
+    soup = BeautifulSoup(webtext)
+    scraped = soup.get_text()
+    return scraped.encode('utf-8')
 
 app = Flask(__name__)
 
@@ -11,11 +16,14 @@ def mainpage():
 
 @app.route('/', methods=['POST'])
 def showresults():
-    results = request.form['search']
-    results = google.search(results, stop=10)
-   # results = google.get_page(results)
-        # [results.extract() for results in soup('script')]
-    return render_template("index.html", results = results)
-    
+    if(re.match('^Who|When', request.form['search'])):
+        ggen = google.search(request.form['search'], stop=1)
+        results = [google.get_page(link) for link in ggen]
+        results = [soupify(x) for x in results]
+        ###INPUT FILTER HERE
+        return render_template("index.html", results = results)
+    return render_template("index.html", results = "Please enter 'Who' or 'When'")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
